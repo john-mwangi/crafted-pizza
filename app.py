b82afffa5b3bbdfadfb61997b3e06b6681255fcf
John Mwangi <mail.mwangi@gmail.com>
2022-11-07 05:14:26 +0300

    Allow user provided data paths

--- app.py
+++ app.py
@@ -1,70 +1,124 @@
 import streamlit as st
 from src.data import import_data, summarise_data
 from datetime import datetime
-import io
-import google.auth
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from googleapiclient.http import MediaIoBaseDownload
-
-# download latest data (zip)
-# unzip folder
-# download data template
-# fill data template with latest data
-# put data template in data folder
-# point app to data folder
-# refresh app
-
-CLIENT_ID = ""
-CLIENT_SECRET = ""
+import pathlib
+import base64
 
 st.title("Crafted Pizza App")
 
-sales_data = import_data.read_sales_data()
-sales_data = import_data.extract_revenue(sales_data)
-sales_data = import_data.extract_sales_date(sales_data)
-sales_summary = summarise_data.summarise_performance(
-    performance_data=sales_data, on_col="revenue"
-)
 
-expenses_data = import_data.read_expenses_data()
-expenses_summary = summarise_data.summarise_performance(
-    performance_data=expenses_data, on_col="expenses"
-)
+download_html = f'<a href="www/Nov2021.xlsx">Data template</a>'
 
-start_period = st.date_input(
-    label="Start Period",
-    value=datetime(year=2020, month=1, day=1),
-)
-start_period = int(start_period.strftime("%Y%m"))
 
-end_period = st.date_input(label="End Period", value=datetime.now())
-end_period = int(end_period.strftime("%Y%m"))
+with st.sidebar:
+    st.write("### Instructions:")
+    st.write(
+        """
+        1. Save sales data for each month in the data template
+        2. Export expenses data for each branch and mark Kabete data with KB
+        3. Save sales data for all months in a single folder
+        4. Save expenses data for all months in another folder
+        5. Enter the paths for the folders below
+        6. Select a start and end period
+        """
+    )
+    st.markdown(download_html, unsafe_allow_html=True)
 
-results = summarise_data.summarise_profit(
-    sales_summary=sales_summary,
-    expenses_summary=expenses_summary,
-    start_period=start_period,
-    end_period=end_period,
-)
+    start_col, end_col = st.columns(2, gap="large")
+    with start_col:
+        start_period = st.date_input(
+            label="Start Period",
+            value=datetime(year=2020, month=1, day=1),
+        )
+        start_period = int(start_period.strftime("%Y%m"))
 
-period_df = results.get("profit_summary")
+    with end_col:
+        end_period = st.date_input(label="End Period", value=datetime.now())
+        end_period = int(end_period.strftime("%Y%m"))
 
-st.write(f"**Period: {start_period} - {end_period}**")
-st.write(f"**Total profit: {results.get('total_profit'):,.0f}**")
+    sales_path = st.text_input(
+        label="Sales path",
+        help="Path to the folder containing sales data e.g. C:/data/sales/",
+    )
 
-st.write(f"Kabete profit: {results.get('KB_profit'):,.0f}")
-st.write(f"Kikuyu profit: {results.get('KK_profit'):,.0f}")
+    expenses_path = st.text_input(
+        label="Expenses path",
+        help="Path to the folder containing expenses data e.g. C:/data/expenses/",
+    )
 
-st.write("*Hit refresh to update the results!*")
-st.dataframe(period_df)
 
-st.download_button(
-    label="Download results",
-    data=period_df.to_csv(index=True),
-    mime="text/csv",
-    file_name="profit_summary.csv",
-)
+@st.cache
+def get_results():
+    sales_data = import_data.read_sales_data(sales_path)
+    sales_data = import_data.extract_revenue(sales_data)
+    sales_data = import_data.extract_sales_date(sales_data)
+    sales_summary = summarise_data.summarise_performance(
+        performance_data=sales_data, on_col="revenue"
+    )
+
+    expenses_data = import_data.read_expenses_data(expenses_path)
+    expenses_summary = summarise_data.summarise_performance(
+        performance_data=expenses_data, on_col="expenses"
+    )
+
+    results = summarise_data.summarise_profit(
+        sales_summary=sales_summary,
+        expenses_summary=expenses_summary,
+        start_period=start_period,
+        end_period=end_period,
+    )
+
+    return results
+
+
+if sales_path == "" or expenses_path == "":
+    st.write("*Enter the paths to the sales and expenses data to start!*")
+
+if sales_path != "" and expenses_path != "":
+    sales_path = pathlib.Path(sales_path).resolve()
+    expenses_path = pathlib.Path(expenses_path).resolve()
+
+    try:
+        results = get_results()
+
+        period_df = results.get("profit_summary")
+
+        rev_col, KK_col, KB_col = st.columns(spec=3, gap="medium")
+        with rev_col:
+            st.metric(
+                label="Total Profit",
+                value=f"{results.get('total_profit'):,.0f}",
+            )
+        with KB_col:
+            st.metric(
+                label="Kabete Profit", value=f"{results.get('KB_profit'):,.0f}"
+            )
+        with KK_col:
+            st.metric(
+                label="Kikuyu Profit", value=f"{results.get('KK_profit'):,.0f}"
+            )
+
+        st.caption("*Hit refresh to update the results!*")
+        st.dataframe(period_df)
+
+        with st.sidebar:
+            st.download_button(
+                label="Download results",
+                data=period_df.to_csv(index=True),
+                mime="text/csv",
+                file_name="profit_summary.csv",
+            )
+
+    except AttributeError:
+        st.error(
+            "*Please check the paths to the sales and expenses data and try again!*"
+        )
+
+
+def authorise_app():
+    """Authorise this app using OAuth2"""
+    # ref: https://www.youtube.com/watch?v=vQQEaSnQ_bs
+    raise NotImplementedError
 
 
 def download_data(real_file_id):
@@ -78,34 +132,5 @@ def download_data(real_file_id):
     TODO(developer) - See https://developers.google.com/identity
     for guides on implementing OAuth2 for the application.
     """
-    creds, _ = google.auth.default()
-
-    try:
-        # create drive api client
-        service = build("drive", "v3", credentials=creds)
-
-        file_id = real_file_id
-
-        # pylint: disable=maybe-no-member
-        request = service.files().get_media(fileId=file_id)
-        file = io.BytesIO()
-        downloader = MediaIoBaseDownload(file, request)
-        done = False
-        while done is False:
-            status, done = downloader.next_chunk()
-            print(f"Download {int(status.progress() * 100)}.")
-
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-        file = None
-
-    return file.getvalue()
-
-
-st.button(
-    label="Download latest data",
-    on_click=download_data,
-    kwargs={"real_file_id": "1xg_YYjGZTyTVFjGYWoeCi7LwZHKta1N2"},
-)
-
-# TODO: Implement Oauth2 authentication
+    # ref: https://developers.google.com/drive/api/v3/manage-downloads
+    raise NotImplementedError
