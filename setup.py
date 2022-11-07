d5536893b3e86df66e9c91270b369edc08bb1fbb
John Mwangi <mail.mwangi@gmail.com>
2022-11-07 23:10:12 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="crafted-pizza-app",
-    packages=find_packages(exclude=["tests*", "tests.*"]),
-    version="1.0.0",
+    name="crafted-pizza",
+    version="1.2.0",
     description="Crafted Pizza App",
+    url="https://github.com/john-mwangi/crafted-pizza",
     author="John Mwangi",
     license="",
     author_email="mail.mwangi@gmail.com",
+    packages=find_packages(exclude=["tests*", "testing*"]),
+    py_modules=["main"],
+    entry_points={"console_scripts": ["crafted-pizza-cli=main:main"]},
 )
